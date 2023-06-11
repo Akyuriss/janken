bb157d26ae76baa31bd9ffdddb924bbbc332b0ad
Akyuriss <taku1006akyuriss@gmail.com>
2023-06-11 12:06:07 +0900

    janken_initialcommit

---
+++ src/janken/Main.java
@@ -0,0 +1,14 @@
+package janken;
+
+
+// import jp.co.trainocate.camp.samples.window.MainWindow;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // ゲームWindowの生成と表示
+        MainWindow window = new MainWindow();
+        window.show();
+    }
+}
